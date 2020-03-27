2f25ba0aedd8d625c12291b3dfd138425d77f1e7
yspdev <yspdev@gmail.com>
2020-03-28 10:04:02 +1100

    0.5

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b22e066deb44e6935e153f34f1d8029d.js"
+  "/precache-manifest.8d9c430930dbd34d079815191b499bcb.js"
 );
 
 workbox.clientsClaim();
