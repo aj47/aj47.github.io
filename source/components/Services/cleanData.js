19623ca76bab5b87149335f69e992d963b391aae
Arash Joobandi <arash@appricot.io>
2021-09-24 18:54:24 +1000

    test appricot site

--- source/components/Services/cleanData.js
+++
@@ -1,8 +0,0 @@
-let FilterData = require('./filter_data3.json');
-
-for(let i in FilterData){
-  FilterData[i] = false;
-}
-
-console.log(JSON.stringify(FilterData));
-
--- source/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './components/Home';
-import RouterApp from './RouterApp';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<RouterApp />, document.getElementById('root'));
-registerServiceWorker();
